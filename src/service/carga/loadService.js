b85be2efb6a0a21f43549ab5b811f3f8114071a5
Rian6 <rian._santos@hotmail.com>
2024-11-24 19:33:07 -0300

    Ajuste nas rotas

--- src/service/carga/loadService.js
+++ src/service/carga/loadService.js
@@ -1,14 +1,14 @@
-import { AUTHSERVICE, MAINSERVICE } from "@root/constants/urls";
+import { LOADSERVICE } from "@root/constants/urls";
 import { get, post } from "@root/service/default/defaultHTTPService";
 
 export function uploadLoads(loads){
-    return post(MAINSERVICE+'/api/sync', loads)
+    return post(LOADSERVICE+'/api/sync', loads)
 }
 
 export function getLoads(idUsuario, isLoadFull=false) {
-    return get(MAINSERVICE + `/api/consumer/${idUsuario}?isLoadFull=${isLoadFull}`);
+    return get(LOADSERVICE + `/api/consumer/${idUsuario}?isLoadFull=${isLoadFull}`);
 }
 
 export function confirmLoads(idUsuario, dataHora, tables) {
-    return post(MAINSERVICE + `/api/consumer-confirm/${idUsuario}?dataHora=${dataHora}`, tables);
+    return post(LOADSERVICE + `/api/consumer-confirm/${idUsuario}?dataHora=${dataHora}`, tables);
 }
